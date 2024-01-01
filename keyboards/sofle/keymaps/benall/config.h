773af3a51c06669d48af40a8b5000553801d4727
Ben Allen <mail@bhallen.co.uk>
2024-01-01 18:04:03 +0000

    Adding sofle custom key maps

---
+++ keyboards/sofle/keymaps/benall/config.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#ifdef TAPPING_TERM
+    #undef TAPPING_TERM
+#endif
+#define TAPPING_TERM 150
+#define QUICK_TAP_TERM 100
+#define TAPPING_TOGGLE 2
+/* The way how "handedness" is decided (which half is which),
+see https://docs.qmk.fm/#/feature_split_keyboard?id=setting-handedness
+for more options.
+*/
